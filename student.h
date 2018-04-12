22b9ed52b3683ceb993ecfc78225c5832c2a69c4
drewatienza <atienza.drew@gmail.com>
2018-04-12 10:42:51 -0700

    created base class student on student.h file

--- student.h
+++ student.h
@@ -2,13 +2,36 @@
 // Created by Drew Atienza on 4/12/18.
 //
 
-#ifndef CLASSROSTER_STUDENT_H
-#define CLASSROSTER_STUDENT_H
+#pragma once
 
+#include <iostream>
+#include <string>
+#include "degree.h"
+using namespace std;
 
 class student {
+private:
+    int Age, DaysInCourse[3];
+    string StudentId, FirstName, LastName, Email;
+    Degree DegreeType;
 
+public:
+    string getStudentId();
+    string getFirstName();
+    string getLastName();
+    string getEmail();
+    int getAge();
+    int *getDaysInCourse();
+    Degree getDegreeType();
+
+    void setStudentId(string);
+    void setFirstName(string);
+    void setLastName(string);
+    void setEmail(string);
+    void setAge(int);
+    void setDaysInCourse(int[3]);
+    void setDegreeType(Degree);
+
+    virtual void print();
+    virtual Degree getDegreeProgram();
 };
-
-
-#endif //CLASSROSTER_STUDENT_H
