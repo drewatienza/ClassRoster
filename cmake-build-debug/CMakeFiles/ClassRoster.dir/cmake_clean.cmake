1364354cae64d85a291381274d87d465eccb85d8
drewatienza <atienza.drew@gmail.com>
2018-04-12 10:19:28 -0700

    initial commit

---
+++ cmake-build-debug/CMakeFiles/ClassRoster.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClassRoster.dir/main.cpp.o"
+  "ClassRoster.pdb"
+  "ClassRoster"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ClassRoster.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ClassRoster.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ClassRoster.
+# This may be replaced when dependencies are built.
